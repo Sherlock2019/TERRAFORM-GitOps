ab6d36a4a3d1f1af1ffb2e1cad37b49e438637cf
SHERLOCK2019 <36204380+Sherlock2019@users.noreply.github.com>
2023-11-24 12:49:01 +0700

    Create main.tf

---
+++ terraform/ modules/kinesis/main.tf
@@ -0,0 +1,11 @@
+resource "aws_kinesis_stream" "example" {
+  name             = "example-kinesis-stream"
+  shard_count      = 1
+  retention_period = 24
+
+  shard_level_metrics = [
+    "IncomingBytes",
+    "OutgoingBytes",
+  ]
+}
+
