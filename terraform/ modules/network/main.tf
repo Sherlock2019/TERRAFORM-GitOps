7f11c4daba103181f0f4af6ce4f42e2963e24b6e
SHERLOCK2019 <36204380+Sherlock2019@users.noreply.github.com>
2023-11-24 12:46:26 +0700

    Create main.tf

---
+++ terraform/ modules/network/main.tf
@@ -0,0 +1,13 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidr
+  availability_zone = var.availability_zone
+}
+
+# Additional resources like Internet Gateway, Route Tables, etc.
